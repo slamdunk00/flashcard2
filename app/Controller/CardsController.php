fd3dd75a57c49e6e3c536e9de3d75067173d0810
LeeYO <bom1442@gmail.com>
2015-05-05 09:54:16 +0700

    asdas
    dasdasdad

--- app/Controller/CardsController.php
+++ app/Controller/CardsController.php
@@ -4,6 +4,8 @@ class CardsController extends AppController {
 	/* var $uses = [ 'User' ]; */
 	 var $uses = [ 'Deck','Card','File', 'Utility','Favourite' ]; 
 
+
+
  
     public function index( $deck_id=null ){
 		$deck_id = $this->params['url']['deck_id'];
@@ -47,82 +49,19 @@ class CardsController extends AppController {
 		$deck_id = $this->params['url']['deck_id'];
 		$this->set('deck_id', $deck_id);
 		
-
-
+		
+		
+			$d_id = isset($this->request->query['$d_id']) ? $this->request->query['$d_id'] : null;
 	        if ($this->request->is('post')) {
-				$this->Card->create();
-				
-				$permitted = array('image/gif','image/jpeg','image/pjpeg','image/png');
-				$typeOK1 = false;
-
-				// check filetype is ok
-				foreach($permitted as $type) {
-				if($type == $this->request->data['Card']['frontpic_update']['type']) {
-				$typeOK1 = true;
-
-				break;}}
-				$permitted = array('image/gif','image/jpeg','image/pjpeg','image/png');
-				$typeOK2 = false;
-				foreach($permitted as $type) {
-				if($type == $this->request->data['Card']['backpic_update']['type']) {
-				$typeOK2 = true;
-
-				break;}
-				}
-				$frontpic_update = null;
-				$backpic_update = null;
-				
-if(($typeOK1) && (typeOK2)) {
-if (
-    !empty($this->request->data['Card']['frontpic_update']['tmp_name'])
-    && is_uploaded_file($this->request->data['Card']['frontpic_update']['tmp_name'])
-) {
-    // Strip path information
-    $frontpic_update = basename($this->request->data['Card']['frontpic_update']['name']); 
-    move_uploaded_file(
-        $this->data['Card']['frontpic_update']['tmp_name'],
-        'cardImg/'.$frontpic_update
-    );
-}
-if (
-    !empty($this->request->data['Card']['backpic_update']['tmp_name'])
-    && is_uploaded_file($this->request->data['Card']['backpic_update']['tmp_name'])
-) {
-    // Strip path information
-    $backpic_update = basename($this->request->data['Card']['backpic_update']['name']); 
-    move_uploaded_file(
-        $this->data['Card']['backpic_update']['tmp_name'],
-        'cardImg/'.$backpic_update
-    );
-}
-
-$frontpath = '/cardImg/'.$frontpic_update;
-$this->request->data['Card']['frontpic'] = $frontpath;
-
-$backpath = '/cardImg/'.$backpic_update;
-$this->request->data['Card']['backpic'] = $backpath;
-
-			
+            $this->Card->create();
             if ($this->Card->save($this->request->data)) {
                 $this->redirect(array('action' => 'index','?' => array('deck_id' => $deck_id )));
 				$this->Session->setFlash(__('The card has been created'));
-
-				}else{
-                    $this->Session->setFlash(__('Unable to Created your Card.'));
-                }
-}else{
-	             if ($this->Card->save($this->request->data)) {
                 $this->redirect(array('action' => 'index','?' => array('deck_id' => $deck_id )));
-				$this->Session->setFlash(__('The card has been created'));
-
 				}else{
                     $this->Session->setFlash(__('Unable to Created your Card.'));
                 }
-}
-
-
-
-} 
+            } 
         }
     
 
@@ -134,89 +73,15 @@ $this->request->data['Card']['backpic'] = $backpath;
 		$deck_id = $this->params['url']['deck_id'];
 		$this->set('deck_id', $deck_id);
 
-		
-		
 		$card = $this->Card->find('first',
 			array(
 			'conditions' => ['Card.id' => $card_id]));
 			
 			$this->set('card', $card);
-			
-
 		
 
             if ($this->request->is('post') || $this->request->is('put')) {
                 $this->Card->id = $card_id;
-				
-						$file = new File(WWW_ROOT . $card['Card']['frontpic'], false, 0777);
-		$file->delete();
-		$file->close();
-		
-		$file = new File(WWW_ROOT . $card['Card']['backpic'], false, 0777);
-		$contents = $file->delete();
-		$file->close();
-				
-				$permitted = array('image/gif','image/jpeg','image/pjpeg','image/png');
-				$typeOK1 = false;
-
-				// check filetype is ok
-				foreach($permitted as $type) {
-				if($type == $this->request->data['Card']['frontpic_update']['type']) {
-				$typeOK1 = true;
-
-				break;}}
-				$permitted = array('image/gif','image/jpeg','image/pjpeg','image/png');
-				$typeOK2 = false;
-				foreach($permitted as $type) {
-				if($type == $this->request->data['Card']['backpic_update']['type']) {
-				$typeOK2 = true;
-
-				break;}
-				}
-				$frontpic_update = null;
-				$backpic_update = null;
-				
-if(($typeOK1) && (typeOK2)) {
-if (
-    !empty($this->request->data['Card']['frontpic_update']['tmp_name'])
-    && is_uploaded_file($this->request->data['Card']['frontpic_update']['tmp_name'])
-) {
-    // Strip path information
-    $frontpic_update = basename($this->request->data['Card']['frontpic_update']['name']); 
-    move_uploaded_file(
-        $this->data['Card']['frontpic_update']['tmp_name'],
-        'cardImg/'.$frontpic_update
-    );
-}
-if (
-    !empty($this->request->data['Card']['backpic_update']['tmp_name'])
-    && is_uploaded_file($this->request->data['Card']['backpic_update']['tmp_name'])
-) {
-    // Strip path information
-    $backpic_update = basename($this->request->data['Card']['backpic_update']['name']); 
-    move_uploaded_file(
-        $this->data['Card']['backpic_update']['tmp_name'],
-        'cardImg/'.$backpic_update
-    );
-}
-
-$frontpath = '/cardImg/'.$frontpic_update;
-$this->request->data['Card']['frontpic'] = $frontpath;
-
-$backpath = '/cardImg/'.$backpic_update;
-$this->request->data['Card']['backpic'] = $backpath;
-
-			
-				if ($this->Card->save($this->request->data)) {
-					$this->Session->setFlash(__('Card has been Edit'));
-                    $this->redirect(array('controller' => 'cards','action' => 'index', '?' => array('deck_id' => $deck_id ) ));
-                }else{
-                    $this->Session->setFlash(__('Unable to Edit your Card.'));
-                }
-}else{
-				
-				
-				
                 if ($this->Card->save($this->request->data)) {
                     $this->Session->setFlash(__('Card has been Edit'));
                     $this->redirect(array('controller' => 'cards','action' => 'index', '?' => array('deck_id' => $deck_id ) ));
@@ -225,8 +90,8 @@ $this->request->data['Card']['backpic'] = $backpath;
                 }
             }
 			
-		}}
-	public function delete($card_id=null) {	
+    }
+	public function delete() {	
 	
 	
 		$card_id = $this->params['url']['card_id'];
@@ -234,23 +99,8 @@ $this->request->data['Card']['backpic'] = $backpath;
 		$deck_id = $this->params['url']['deck_id'];
 		$this->set('deck_id', $deck_id);
 		
-			$card = $this->Card->find('first',
-							array(
-								'conditions' => [
-									'Card.id' => $card_id
-								]
-							)
-			);
-		
 		$this->Card->id = $card_id;
 		$this->Card->delete();
-		$file = new File(WWW_ROOT . $card['Card']['frontpic'], false, 0777);
-		$file->delete();
-		$file->close();
-		
-		$file = new File(WWW_ROOT . $card['Card']['backpic'], false, 0777);
-		$contents = $file->delete();
-		$file->close();
 		
 		$this->redirect(array('controller' => 'cards','action' => 'index', '?' => array('deck_id' => $deck_id ) ));
 			
--- app/View/Cards/index.ctp
+++ app/View/Cards/index.ctp
@@ -21,6 +21,7 @@
 	</table>
 	
 <p></p>
+
 <?php 	if($this->Session->check('Auth.User')){
 		if( $role == 'a'){
 echo $this->Html->link( " Add New Card ",   array('controller'=>'cards','action'=>'add' ,'?' => array('deck_id' => $deck_id )) ),'&nbsp'; 
@@ -60,30 +61,16 @@ echo $this->SocialShare->link('facebook',__('Share on Facebook'));
 <?php foreach($cards as $card): ?>
  <div class="flip-container" ontouchstart="this.classList.toggle('hover');" id="card-container">
 	<div class="flipper">
-
 <?php
-	$front = $card['Card']['frontpic'];
-	$back = $card['Card']['backpic'];
-	echo '<div>';
+	$front = $card['Card']['front'];
 	echo	'<div class="front">';
-	echo	 $card['Card']['front'];
+	echo	 $this->Html->image($front, array('width' => '50px','alt'=>$front));
 	echo	'</div>';
 	echo	'<div class="back">';
 	echo	$card['Card']['back'];
 	echo	'</div>';
-	echo '</div>';
-	echo '<div>';
-	if($card['Card']['frontpic'] != null){
-	echo	'<div class="front">';
-	echo	 $this->Html->image($front, array('width' => '50px','alt'=>''));
-	echo	'</div>';}
-	if($card['Card']['backpic'] != null){
-	echo	'<div class="back">';
-	echo	$this->Html->image($back, array('width' => '50px','alt'=>''));
-	echo	'</div>';}
-	echo 	'</div>';
 ?>	
-
+	
 		</div>
 </div>
 
@@ -96,21 +83,10 @@ echo $this->SocialShare->link('facebook',__('Share on Facebook'));
 
 <p></p>
 <div>
-<?php 
-	if($this->Session->check('Auth.User')){
-	if( $role == 'a'){
-	echo $this->Html->link( " Edit Card ",   array('controller'=>'cards','action'=>'edit' ,'?' => array('deck_id' => $deck_id , 'card_id' => $card['Card']['id'], )) ),'&nbsp&nbsp;' ;
-	echo $this->Html->link( " Delete Card ",   array('controller'=>'cards','action'=>'delete' ,'?' => array('deck_id' => $deck_id , 'card_id' => $card['Card']['id'] )),
-    array('confirm' => 'Are you sure you want to delete this Card?') );
-	}else{
-	if ($user_id == $decks['Deck']['user_id'] ){
-	echo $this->Html->link( " Edit Card ",   array('controller'=>'cards','action'=>'edit' ,'?' => array('deck_id' => $deck_id , 'card_id' => $card['Card']['id'], )) ),'&nbsp&nbsp;'; 
-	echo $this->Html->link( " Delete Card ",   array('controller'=>'cards','action'=>'delete' ,'?' => array('deck_id' => $deck_id , 'card_id' => $card['Card']['id'] )),
-    array('confirm' => 'Are you sure you want to delete this Card?') );
-	}}
-	}?>
 
-
+	<?php echo $this->Html->link( " Edit Card ",   array('controller'=>'cards','action'=>'edit' ,'?' => array('deck_id' => $deck_id , 'card_id' => $card['Card']['id'], )) ),'&nbsp&nbsp;' ?>
+	<?php echo $this->Html->link( " Delete Card ",   array('controller'=>'cards','action'=>'delete' ,'?' => array('deck_id' => $deck_id , 'card_id' => $card['Card']['id'] )),
+    array('confirm' => 'Are you sure you want to delete this Card?') )?>
 	
 </div>
 
